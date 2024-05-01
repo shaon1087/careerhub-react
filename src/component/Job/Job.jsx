448e852fbe8e6508c6683f56e326df5341b7bf7d
shaon1087 <shaonsaha1087@gmail.com>
2024-05-01 18:14:25 +0600

    job file added

--- src/component/Job/Job.jsx
+++ src/component/Job/Job.jsx
@@ -1,16 +1,31 @@
+import PropTypes from "prop-types";
 
+const Job = ({ job }) => {
+  const {
+    logo,
+    job_title,
+    company_name,
+    remote_or_onsite,
+    location,
+    job_type,
+    salary,
 
-const Job = ({job}) => {
+  } = job;
+  return (
+    <div>
+      <div>
+        <img src={logo} alt="" />
+        <h3 className="text-xl ">{job_title}</h3>
+        <h2>{ company_name}</h2>
+        
 
-
-    const {logo} = job;
-    return (
-        <div>
-            <img src={logo} alt="" />
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
-propT
+Job.propTypes = {
+  job: PropTypes.object.isRequired
+};
 
 export default Job;

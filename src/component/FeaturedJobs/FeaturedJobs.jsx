5b71375bcbe7feebaa0087852daaa5784bee88c6
shaon1087 <shaonsaha1087@gmail.com>
2024-05-01 12:24:43 +0600

    featured and category part added

--- src/component/FeaturedJobs/FeaturedJobs.jsx
+++ src/component/FeaturedJobs/FeaturedJobs.jsx
@@ -1,12 +1,29 @@
+import { useEffect, useState } from "react";
+import Job from "../Job/Job";
 
 const FeaturedJobs = () => {
+    const [jobs,setJobs] = useState([]);
+    useEffect(() =>{
+        fetch('jobs.json')
+        .then(res => res.json())
+        .then(data => setJobs(data));
+    
+    },[]);
+
     return (
-      <div className="items-center text-center">
-        <h2 className="text-5xl">Featured Jobs</h2>
-        <p className="text-sm">
-          Explore thousands of job opportunities with all the information you
-          need. Its your future
-        </p>
+      <div>
+        <div className="items-center text-center">
+          <h2 className="text-5xl">Featured Jobs: {jobs.length}</h2>
+          <p className="text-sm">
+            Explore thousands of job opportunities with all the information you
+            need. Its your future
+          </p>
+        </div>
+        <div>
+            {
+                jobs.map(job => <Job key={job.id} job={job}></Job>)
+            }
+        </div>
       </div>
     );
 };
---
+++ src/component/Job/Job.jsx
@@ -0,0 +1,16 @@
+
+
+const Job = ({job}) => {
+
+
+    const {logo} = job;
+    return (
+        <div>
+            <img src={logo} alt="" />
+        </div>
+    );
+};
+
+propT
+
+export default Job;

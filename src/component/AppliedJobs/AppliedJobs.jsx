f6f8a4143b70a6020eaac2bbf0776f1d89086ae2
shaon1087 <shaonsaha1087@gmail.com>
2024-05-03 16:01:15 +0600

    all work done here

--- src/component/AppliedJobs/AppliedJobs.jsx
+++ src/component/AppliedJobs/AppliedJobs.jsx
@@ -4,6 +4,21 @@ import { getStoredApplication } from "../../Utility/localStorage";
 
 const AppliedJobs = () => { 
   const [appliedJobs,setAppliedJobs] = useState([]);
+  const [displayJobs,setDisplayJobs] = useState([]);
+
+  const handleJobsFilter = filter =>{
+    if(filter==='all'){
+      setDisplayJobs(appliedJobs);
+  }
+   else if(filter==='remote'){
+      const remoteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Remote');
+      setDisplayJobs(remoteJobs);
+  }
+   else if(filter==='onsite'){
+      const onsiteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Onsite');
+       setDisplayJobs(onsiteJobs);
+  }}
+    
 
   const jobs = useLoaderData();
   useEffect(()=> {
@@ -20,9 +35,9 @@ const AppliedJobs = () => {
       
       
       setAppliedJobs(jobApplied);
-      console.log(jobs, storedJobs, jobApplied);
+      setDisplayJobs(jobApplied);
     }
-  },[])
+  },[jobs]);
 
 
   return (
@@ -31,26 +46,28 @@ const AppliedJobs = () => {
       <details className="dropdown">
         <summary className="m-1 btn">open or close</summary>
         <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-          <li>
+          <li onClick={()=> handleJobsFilter('all')}>
             <a>All</a>
           </li>
-          <li>
+          <li onClick={()=> handleJobsFilter('remote')}>
             <a>Remote</a>
           </li>
-          <li>
+          <li onClick={()=> handleJobsFilter('onsite')}>
             <a>Onsite</a>
           </li>
         </ul>
       </details>
 
       <ul>
-        {appliedJobs.map((job) => (
+        {
+        displayJobs.map((job) => (
           <li key={job.id}>
             <span>
               {job.job_title}: {job.remote_or_onsite}
             </span>
           </li>
-        ))}
+        ))
+        }
       </ul>
     </div>
   );

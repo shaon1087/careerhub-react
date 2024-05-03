9df136b91defdf426d768f9f4d736d1b7c4333d6
shaon1087 <shaonsaha1087@gmail.com>
2024-05-03 15:28:22 +0600

    applied job data show

--- src/component/AppliedJobs/AppliedJobs.jsx
+++ src/component/AppliedJobs/AppliedJobs.jsx
@@ -28,16 +28,30 @@ const AppliedJobs = () => {
   return (
     <div>
       <h1>jobs i applied: {appliedJobs.length}</h1>
-
-      
-        <ul>
-          {
-              appliedJobs.map(job => <li key={job.id} >
-                <span>{job.job_title}</span>
-              </li>)
-          }
+      <details className="dropdown">
+        <summary className="m-1 btn">open or close</summary>
+        <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+          <li>
+            <a>All</a>
+          </li>
+          <li>
+            <a>Remote</a>
+          </li>
+          <li>
+            <a>Onsite</a>
+          </li>
         </ul>
-      
+      </details>
+
+      <ul>
+        {appliedJobs.map((job) => (
+          <li key={job.id}>
+            <span>
+              {job.job_title}: {job.remote_or_onsite}
+            </span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
